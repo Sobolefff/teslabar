61d4451f064a534690586d04f44e4fe6221c3deb
Sobolefff <89976955+Sobolefff@users.noreply.github.com>
2022-06-07 06:43:42 +0300

    верстка слайдера

---
+++ src/components/const.js
@@ -0,0 +1,20 @@
+const gallery = [
+    {
+      src: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      src: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      src: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      src: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      src: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      src: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,37 @@
 import '../pages/index.css';
+
+//slider
+const container = document.querySelector('.gallery__slider');
+const slider = document.querySelector('.gallery__sliderline');
+const slide = document.querySelector('.gallery__slide');
+const slides = document.querySelectorAll('.gallery__slide');
+const arr = [];
+const btnPrev = document.querySelector('.gallery__button-left');
+const btnNext = document.querySelector('.gallery__button-right');
+let offset = 0;
+let step = 0;
+for (let i=0; i<slides.length; i++) {
+  arr[i] = slides[i].src;
+  slides[i].remove();
+  draw();
+}
+console.log(arr);
+
+btnPrev.addEventListener('click', () => {
+  offset += 100;
+  slider.style.left = offset + 'px';
+  //arr.push(arr.shift());
+  arr.unshift(arr.pop());
+  slider.lastElementChild.remove();
+  draw();
+  
+  console.log(arr);
+});
+function draw() {
+  let img = document.createElement('img');
+  img.src = arr[step];
+  img.classList.add('slide');
+  slider.prepend(img);
+  step++;
+  console.log(img.src);
+};
