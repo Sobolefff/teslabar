558b5400037d72614f69cc102f3e1ad6c1fdb2f8
Sobolefff <89976955+Sobolefff@users.noreply.github.com>
2022-06-11 16:03:29 +0300

    done all

--- src/components/index.js
+++ src/components/index.js
@@ -8,6 +8,18 @@ popup();
 carouselMobile();
 shiftTop();
 
-document.querySelector('.burger-icon').addEventListener('click', () => {
-    document.querySelector('.mobile-menu').classList.toggle('mobile-menu_opened');
+const burgerIcon = document.querySelector('.burger-icon');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+/*burgerIcon.addEventListener('click', () => {
+    mobileMenu.classList.toggle('mobile-menu_opened');
+});*/
+
+window.addEventListener('click', (evt) => {
+    if (evt.target.classList.contains('burger-icon') || evt.target.classList.contains('mobile-menu_opened')) {
+        mobileMenu.classList.add('mobile-menu_opened');
+    } else {
+        mobileMenu.classList.remove('mobile-menu_opened');
+    }
 });
+
