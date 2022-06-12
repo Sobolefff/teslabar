9f81ee804581699120def02fe3816c3238d66d30
Sobolefff <89976955+Sobolefff@users.noreply.github.com>
2022-06-12 12:10:37 +0300

    seo

--- src/components/index.js
+++ src/components/index.js
@@ -11,15 +11,11 @@ shiftTop();
 const burgerIcon = document.querySelector('.burger-icon');
 const mobileMenu = document.querySelector('.mobile-menu');
 
-/*burgerIcon.addEventListener('click', () => {
-    mobileMenu.classList.toggle('mobile-menu_opened');
-});*/
-
-window.addEventListener('click', (evt) => {
+document.addEventListener('click', (evt) => {
     if (evt.target.classList.contains('burger-icon') || evt.target.classList.contains('mobile-menu_opened')) {
         mobileMenu.classList.add('mobile-menu_opened');
+        document.removeEventListener('click', evt);
     } else {
         mobileMenu.classList.remove('mobile-menu_opened');
     }
 });
-
