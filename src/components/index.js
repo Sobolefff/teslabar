ee01791c87e9a26cb367596bdfd0d712357ccb34
Sobolefff <89976955+Sobolefff@users.noreply.github.com>
2022-06-14 11:46:27 +0300

    правка метрики

--- src/components/index.js
+++ src/components/index.js
@@ -11,6 +11,15 @@ import { shiftTop } from './topBtn.js';
 
 setTimeout(
     () => {
+        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+    m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+ 
+    ym(89172076, "init", {
+         clickmap:true,
+         trackLinks:true,
+         accurateTrackBounce:true
+    });
         carousel();
         popup();
         carouselMobile();
